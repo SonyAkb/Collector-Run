8ee0cfefe47c4a667f5e036f1e82243dfe012dac
Sonya <sofiaakb13391@gmail.com>
2025-08-04 14:32:48 +0500

    keeping the record. adding a record reset button and an exit button to the start menu.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,12 +13,19 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI timerText; //таймер
 
     public TextMeshProUGUI endGameText; //текст в конце раунда
+    public TextMeshProUGUI bestScoreText; //рекорд
 
     public GameObject gameOverPanel;
 
     private int score = 0; //первоначальный счет
     private float remainingTime = 10f;//первоначальное время
     private bool isGameActive = true; //флаг игры - игра идет или закончилась
+
+    void Start()
+    {
+        UpdateBestScoreUI(); //данные о последнем рекорде
+        
+    }
 
     void Awake()
     {
@@ -41,13 +49,27 @@ public class GameManager : MonoBehaviour
     {
         score += value;
         scoreText.text = $"Монет собрано: {score}";
+
+        if (score > PlayerPrefs.GetInt("BestScore", 0)) //обновляю рекорд
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+            UpdateBestScoreUI(); 
+        }
+    }
+
+    void UpdateBestScoreUI()
+    {
+        if (bestScoreText != null)
+        {
+            bestScoreText.text = $"Рекорд: {PlayerPrefs.GetInt("BestScore", 0)}";
+        }
     }
 
     void EndGame()
     {
         if (endGameText != null)
         {
-            endGameText.text = $"Игра окончена! Монет собрано {score}";
+            endGameText.text = $"Игра окончена! Монет собрано {score}\nРекорд: {PlayerPrefs.GetInt("BestScore", 0)}";
         }
         else
         {
@@ -72,6 +94,7 @@ public class GameManager : MonoBehaviour
 
     public void ReturnToMenu() //в главное меню
     {
+        PlayerPrefs.Save();
         Time.timeScale = 1; 
         SceneManager.LoadScene("MenuScene"); 
     }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,13 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    
+    public TextMeshProUGUI bestScoreText; //рекорд
+
+    void Start()
+    {
+        UpdateBestScoreUI();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene"); 
+    }
+
+    void UpdateBestScoreUI() //обновление рекорда
+    {
+        if (bestScoreText != null)
+        {
+            bestScoreText.text = $"Рекорд: {PlayerPrefs.GetInt("BestScore", 0)}";
+        }
+    }
+
+    public void ResetRecord() //сброс рекорда
+    {
+        PlayerPrefs.DeleteKey("BestScore");
+        UpdateBestScoreUI(); //обновляю текст
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false; // Остановка в редакторе
+        #else
+                    Application.Quit(); // Выход в собранной версии
+        #endif
     }
 }
