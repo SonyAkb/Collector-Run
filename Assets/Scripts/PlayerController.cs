7329d939206dca39b3f6e1baa48db0a98fe2aff3
Sonya <sofiaakb13391@gmail.com>
2025-08-04 17:11:47 +0500

    Adding borders. The player can no longer leave the playing field

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     public float speedPlayer = 7f; //скорость игрока
     private Rigidbody2D rb; //отвечает за физику объекта
+
+    private Vector2 lastNormalPosition; //последняя нормальная позицая - до выхода за границу
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        lastNormalPosition = transform.position;
     }
     
     void Update()
@@ -17,6 +21,27 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");//A/D, ←/→
         float moveY = Input.GetAxis("Vertical");//W/S, ↑/↓
 
-        rb.velocity = new Vector2(moveX * speedPlayer, moveY * speedPlayer);//задает скорость
+        rb.velocity = new Vector2(moveX * speedPlayer, moveY * speedPlayer);//перемещение
     }
+
+    void OnTriggerExit2D(Collider2D other) //при входе в триггер
+    {
+        if (other.gameObject.name == "Boundaries")
+        {
+            Vector2 clampedPosition = new Vector2(
+                Mathf.Clamp(transform.position.x, -other.bounds.extents.x, other.bounds.extents.x),
+                Mathf.Clamp(transform.position.y, -other.bounds.extents.y, other.bounds.extents.y));
+            transform.position = lastNormalPosition; //возврат в последнее нормальное положение
+            rb.velocity = Vector2.zero; //сброс скорости
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other) //внутри границ
+    {
+        if (other.gameObject.name == "Boundaries")
+        {
+            lastNormalPosition = transform.position;
+        }
+    }
+
 }
